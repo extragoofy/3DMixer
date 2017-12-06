6788259dd88bf05fb43acb78bc7f54caa299cb85
Mia Wölm <mia.woelm@uptain.de>
2017-12-06 22:24:25 +0100

    get midi file in javascript

---
+++ Midi Test/midi.js
@@ -0,0 +1,20 @@
+//let frequencies = [];
+
+let request = new XMLHttpRequest(); 
+request.open('GET', "../instruments/moog bass.mid", true); 
+request.responseType = 'arraybuffer';
+request.onload = function () {
+    console.log(request.response);
+    let undecodedAudio = request.response; 
+    if (request.response instanceof ArrayBuffer){
+			let byteArray = new Uint8Array(request.response);
+            console.log(byteArray);
+//            for(let i = 0; i < byteArray.length; i++) {
+//                if(byteArray[i] === 144){
+//                    frequency = midiNoteToFrequency(byteArray[i+1]);
+//                    frequencies.push(frequency);
+//                }
+//            }
+		}
+};
+request.send();
