097e4ae450d9738f63170f6c9f964603584fbd13
Mia Wölm <mia.woelm@uptain.de>
2017-12-06 23:37:02 +0100

    Play given midi (without any effects)

--- Midi Test/midi.js
+++ Midi Test/midi.js
@@ -2,11 +2,17 @@ let frequencies = [];
 let ppq;
 let mpqn = '';
 let bpm;
-let startTime = 0;
 let currentTime = 0;
 
+let startTime;
+let nextNotetime;
+
+let context = new AudioContext();
+let oscillator;
+let currentNote = 0;
+
 let request = new XMLHttpRequest(); 
-request.open('GET', "../instruments/moog bass.mid", true); 
+request.open('GET', "song1_flute.mid", true); 
 request.responseType = 'arraybuffer';
 request.onload = function () {
     console.log(request.response);
@@ -28,10 +34,12 @@ request.onload = function () {
                 }
                 // get midi notes
                 if(byteArray[i] === 144){
+                    // if it's the first note, check how many beats will be played before playing that note
                     if(firstNote){
                         currentTime += (60 / (bpm * ppq)) * byteArray[i-1];
                         firstNote = false;
                     }
+                    
                     frequency = midiNoteToFrequency(byteArray[i+1]);
                     const duration = (60 / (bpm * ppq)) * byteArray[i+3];
                     frequencies.push({
@@ -40,6 +48,8 @@ request.onload = function () {
                         time: currentTime,
                     });
                     currentTime += duration;
+                    
+                    // track note pauses
                     if(byteArray[i+4] === 128 && byteArray[i+1] === byteArray[i+5]){
                         const pause = (60 / (bpm * ppq)) * byteArray[i+7];
                         currentTime += pause;
@@ -53,4 +63,35 @@ request.send();
 
 function midiNoteToFrequency (note) {
     return Math.pow(2, ((note - 69) / 12)) * 440;
+}
+
+document.getElementById("play").addEventListener('click', function(){
+    nextNotetime = context.currentTime;
+    startTime = context.currentTime;
+    scheduler();
+});
+
+function scheduler() {
+    var secondsPerBeat = 60.0 / bpm;
+
+    while(nextNotetime < context.currentTime + 0.1) {
+        nextNotetime += 0.24 * secondsPerBeat;
+        playSound(startTime + frequencies[currentNote].time);
+       
+    }
+
+   timerID = window.setTimeout(scheduler, 25.0);
+}
+
+function playSound(time){
+    if(currentNote === frequencies.length){
+        currentNote = 0;
+        clearTimeout(timerID);
+    }
+    oscillator = context.createOscillator();
+    oscillator.frequency.value = frequencies[currentNote].frequency;
+    oscillator.connect(context.destination);
+    oscillator.start(time);
+    oscillator.stop(time + frequencies[currentNote].duration);
+    currentNote++;
 }
