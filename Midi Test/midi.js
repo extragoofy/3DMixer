7d4ccadec9a7e5f0452abe1c4d24eb00f6f4a978
Mia Wölm <mia.woelm@uptain.de>
2017-12-06 22:25:38 +0100

    Calculate Frequencies based on the midi signal

--- Midi Test/midi.js
+++ Midi Test/midi.js
@@ -1,4 +1,4 @@
-//let frequencies = [];
+let frequencies = [];
 
 let request = new XMLHttpRequest(); 
 request.open('GET', "../instruments/moog bass.mid", true); 
@@ -9,12 +9,16 @@ request.onload = function () {
     if (request.response instanceof ArrayBuffer){
 			let byteArray = new Uint8Array(request.response);
             console.log(byteArray);
-//            for(let i = 0; i < byteArray.length; i++) {
-//                if(byteArray[i] === 144){
-//                    frequency = midiNoteToFrequency(byteArray[i+1]);
-//                    frequencies.push(frequency);
-//                }
-//            }
+            for(let i = 0; i < byteArray.length; i++) {
+                if(byteArray[i] === 144){
+                    frequency = midiNoteToFrequency(byteArray[i+1]);
+                    frequencies.push(frequency);
+                }
+            }
 		}
 };
 request.send();
+
+function midiNoteToFrequency (note) {
+    return Math.pow(2, ((note - 69) / 12)) * 440;
+}
